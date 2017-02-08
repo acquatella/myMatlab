2ac7b692dc8caa023d3dfe0f6c0229a3205958e5
acquatella <paul@acquatella.com>
2017-02-08 16:42:36 +0100

    modifs

--- scripts/plots/plotting_stairs.m
+++ scripts/plots/plotting_stairs.m
@@ -1,5 +1,3 @@
-% This is an example of how to plot multi-arrays in stairs
-
 % This program is free software: you can redistribute it and/or modify
 % it under the terms of the GNU General Public License as published by
 % the Free Software Foundation, either version 3 of the License, or
@@ -13,20 +11,22 @@
 % You should have received a copy of the GNU General Public License
 % along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+
+% This is an example of how to plot multi-arrays in stairs:
 % Let´s say you have a vector array of torques 'tau' with size [3x1]
-% and you also have a time vector of appropiate size.
+% and you also have a time vector 't' of appropiate size.
 % A nice way to plot these in stairs without using 'hold on' is:
 
 figure;
     Y = [tau(1,:); tau(2,:); tau(3,:)];
-    h = stairs(t',Y');
+    h = stairs(t', Y');
     set(h(1),'color','b','LineStyle','-', 'Linewidth',1.5) 
     set(h(2),'color','g','LineStyle','--','Linewidth',1.5)
     set(h(3),'color','r','LineStyle','-.','Linewidth',1.5)
-    set(h(2),'color',get(h(2),'color')*0.4);
-    set(h(3),'color',get(h(3),'color')*0.9);
     setunits('s','Nm');
     title('Torques (t)');
     legend('Torque x','Torque y','Torque z','location','NorthEast');
     setfigtempl;
-    as = axis; axis([as(1) as(2) -1.05 1.05]);
+    as = axis; axis([as(1) as(2) axis_min axis_max]);
+	
+% eof
