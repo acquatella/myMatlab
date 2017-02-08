16c0b15e56a6a48e18b6b54a8193f0e6e58a3780
acquatella <paul@acquatella.com>
2017-02-08 16:25:00 +0100

    plotting stairs

---
+++ scripts/plots/plottng_stairs.m
@@ -0,0 +1,32 @@
+% This is an example of how to plot multi-arrays in stairs
+
+% This program is free software: you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation, either version 3 of the License, or
+% (at your option) any later version.
+% 
+% This program is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+% 
+% You should have received a copy of the GNU General Public License
+% along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+% Let´s say you have a vector array of torques 'tau' with size [3x1]
+% and you also have a time vector of appropiate size.
+% A nice way to plot these in stairs without using 'hold on' is:
+
+figure;
+    Y = [tau(1,:); tau(2,:); tau(3,:)];
+    h = stairs(t',Y');
+    set(h(1),'color','b','LineStyle','-', 'Linewidth',1.5) 
+    set(h(2),'color','g','LineStyle','--','Linewidth',1.5)
+    set(h(3),'color','r','LineStyle','-.','Linewidth',1.5)
+    set(h(2),'color',get(h(2),'color')*0.4);
+    set(h(3),'color',get(h(3),'color')*0.9);
+    setunits('s','Nm');
+    title('Torques (t)');
+    legend('Torque x','Torque y','Torque z','location','NorthEast');
+    setfigtempl;
+    as = axis; axis([as(1) as(2) -1.05 1.05]);
